ed396b870d706f66510ce44a0addf93d8f854acc
zakur <anastasiyavolkova165@gmail.com>
2020-03-22 14:01:20 +0300

    Добавлен test_add_groop

--- test_add_groop.py
+++ test_add_groop.py
@@ -35,13 +35,13 @@ class TestAddGroop(unittest.TestCase):
         wd.find_element_by_link_text("Logout").click()
     
     def is_element_present(self, how, what):
-       # try: self.wd.find_element(by=how, value=what)
-       # except NoSuchElementException as e: return False
+        try: self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
         return True
     
     def is_alert_present(self):
-       # try: self.wd.switch_to_alert()
-       # except NoAlertPresentException as e: return False
+        try: self.wd.switch_to_alert()
+        except NoAlertPresentException as e: return False
         return True
 
     def tearDown(self):
