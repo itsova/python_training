8bdf2dd35a233d1d715e6d6441d6580e66786edf
zakur <anastasiyavolkova165@gmail.com>
2020-03-22 14:12:27 +0300

    Исправлена опечатка в названии теста test_add_group

--- test_add_group.py
+++ test_add_group.py
@@ -3,12 +3,12 @@ from selenium import webdriver
 import unittest
 
 
-class TestAddGroop(unittest.TestCase):
+class TestAddGroup(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_add_groop(self):
+    def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").click()
