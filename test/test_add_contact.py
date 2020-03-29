4ba9d4492daecbf05dfc1253fc7830daf8ea0c37
zakur <anastasiyavolkova165@gmail.com>
2020-03-29 16:18:29 +0300

    Добавлен тест на удаление группы Запуск в одной сессии Костыль с logout

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import pytest
 from model.contact import Contact
-from fixture.application import Application
 
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
@@ -17,6 +9,7 @@ def test_add_contact(app):
                                title="hfhfghgf", company="ghfghfgh", address="fghfghf",
                                home="fghfgdhf", mobile="gfhfghfgh", email="fghfghfg"))
     app.session.logout()
+
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
