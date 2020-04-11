a70cd535d65fab4378d8880b246d4dd645a4442a
zakur <anastasiyavolkova165@gmail.com>
2020-04-11 20:10:23 +0300

    Кэширование при загрузке списка групп

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,7 +11,7 @@ def test_add_contact(app):
     assert len(old_contacts) +1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-   
+
 
 def test_add_empty_contact(app):
     old_contacts = app.contact.get_contact_list()
