8e615cdb9e50ee25f099e93d3971d9279a78e52c
zakur <anastasiyavolkova165@gmail.com>
2020-04-19 22:22:20 +0300

    Реализованы опции командной строки для указания типа браузера

--- conftest.py
+++ conftest.py
@@ -7,12 +7,15 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
+    password = request.config.getoption("--password")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url)
+    fixture.session.ensure_login(username="admin", password=password)
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
@@ -21,3 +24,8 @@ def stop(request):
         fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", defauil="http://localhost/addressbook/")
+    parser.addoption("--password", action="store", default="secret")
