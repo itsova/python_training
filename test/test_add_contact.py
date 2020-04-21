8ca90b44d0ceaab7143f074d6df43c7be2f7fcd8
zakur <anastasiyavolkova165@gmail.com>
2020-04-21 10:41:04 +0300

    fix параметризации тестов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -15,18 +15,13 @@ def random_string_number(prefix, maxlen):
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 testdata = [
-    Contact(firstname=firstname, lastname=lastname, address=address, homephone=homephone, workphone=workphone,
-            email=email, email2=email2, email3=email3, mobilephone=mobilephone, secondaryphone=secondaryphone)
-    for firstname in ["", random_string("firstname", 10)]
-    for lastname in ["", random_string("lastname", 10)]
-    for address in ["", random_string("address", 20)]
-    for email in ["", random_string("emailr", 15)]
-    for email2 in ["", random_string("email2", 15)]
-    for email3 in ["", random_string("email3", 15)]
-    for homephone in ["", random_string_number("+7", 15)]
-    for mobilephone in ["", random_string_number("+7", 25)]
-    for workphone in ["", random_string_number("+7", 20)]
-    for secondaryphone in ["", random_string_number("+7", 20)]
+    Contact(firstname="", lastname="", address="", homephone="", workphone="",
+            email="", email2="", email3="", mobilephone="", secondaryphone="")] + [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10), address=random_string("address", 20),
+            homephone=random_string_number("+7", 15), workphone=random_string_number("+7", 20),
+            email=random_string("email@", 15), email2=random_string("email2", 15), email3=random_string("email3", 15),
+            mobilephone=random_string_number("+7", 25), secondaryphone=random_string_number("+7", 20))
+    for i in range(4)
 ]
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
@@ -37,7 +32,6 @@ def test_add_contact(app, contact):
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
 
 
 
